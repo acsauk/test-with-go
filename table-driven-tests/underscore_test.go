1d5d40bbce512c59331501e4bd223188903660b2
Alex Saunders <acsauk@gmail.com>
2020-12-03 21:10:15 +0000

    Add initial tdt example

---
+++ table-driven-tests/underscore_test.go
@@ -0,0 +1,23 @@
+package underscore
+
+import "testing"
+
+func TestCamel(t *testing.T) {
+	testCases := []struct{
+		arg string
+		want string
+	}{
+		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+		{"with a space", "with a space"},
+		{"endsWithA", "ends_with_a"},
+	}
+
+	for _, tc := range testCases {
+		got := Camel(tc.arg)
+		if got != tc.want {
+			t.Errorf("Camel(%q) = %q; want %q", tc.arg, got, tc.want)
+		}
+	}
+
+
+}
