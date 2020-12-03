b1ad2fa8d2c2f668e11680ca7a5a0a0ce5f8e631
Alex Saunders <acsauk@gmail.com>
2020-12-03 21:17:34 +0000

    Autogenerate table tests with IDE/Editor

--- table-driven-tests/underscore_test.go
+++ table-driven-tests/underscore_test.go
@@ -1,23 +1,45 @@
 package underscore
 
-import "testing"
+import (
+	"testing"
+)
+
+// func TestCamel(t *testing.T) {
+// 	testCases := []struct{
+// 		arg string
+// 		want string
+// 	}{
+// 		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+// 		{"with a space", "with a space"},
+// 		{"endsWithA", "ends_with_a"},
+// 	}
+
+// 	for _, tc := range testCases {
+// 		got := Camel(tc.arg)
+// 		if got != tc.want {
+// 			t.Errorf("Camel(%q) = %q; want %q", tc.arg, got, tc.want)
+// 		}
+// 	}
+// }
 
 func TestCamel(t *testing.T) {
-	testCases := []struct{
-		arg string
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
 		want string
 	}{
-		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
-		{"with a space", "with a space"},
-		{"endsWithA", "ends_with_a"},
+		{"Simple", args{"thisIsACamelCaseString"}, "this_is_a_camel_case_string"},
+		{"Spaces", args{"with a space"}, "with a space"},
+		{"Ending with A", args{"endsWithA"}, "ends_with_a"},
 	}
-
-	for _, tc := range testCases {
-		got := Camel(tc.arg)
-		if got != tc.want {
-			t.Errorf("Camel(%q) = %q; want %q", tc.arg, got, tc.want)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Camel(tt.args.str); got != tt.want {
+				t.Errorf("Camel() = %v, want %v", got, tt.want)
+			}
+		})
 	}
-
-
 }
